e5461b9bc615876d1c5400ff66888ee883cfdfca
Raj <63090941+newy8@users.noreply.github.com>
2021-12-11 22:43:42 +0530

    Initial commit.

---
+++ NotifyR.Api/Models/UserSubscription.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace NotifyR.Api.Models
+{
+    internal class UserSubscription
+    {
+        public string Id { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public List<WebPushSubscription> WebPushSubscriptions { get; set; }
+    }
+}
---
+++ NotifyR.Api/Models/WebPushSubscription.cs
@@ -0,0 +1,18 @@
+﻿namespace NotifyR.Api.Models
+{
+    internal class WebPushSubscription
+    {
+        public string Endpoint { get; set; }
+
+        public string ExpirationTime { get; set; }
+
+        public Keys Keys { get; set; }
+    }
+
+    internal class Keys
+    {
+        public string P256DH { get; set; }
+
+        public string Auth { get; set; }
+    }
+}
