eddbe184a54d148a47e6b8ca3737b0788f0ea3b5
Raj <63090941+newy8@users.noreply.github.com>
2022-06-15 20:38:41 +0530

    Add project files.

---
+++ NotifyR.Sample.Client/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NotifyR.Sample.Client.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
